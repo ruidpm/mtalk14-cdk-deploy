21a007445915245ce1727ae254e8114fd05379e8
Rui Magalhães <rui.magalhaes@dunelm.com>
2023-03-30 16:30:04 +0100

    add log

--- src/publishNewsLambdaHandler.ts
+++ src/publishNewsLambdaHandler.ts
@@ -9,11 +9,14 @@ export const addSubscriptionLambdaHandler = async (event: APIGatewayEvent) => {
   const body = event.body as unknown as PublishNews;
   const topicArn = process.env.NEWS_TOPIC as string;
 
+  console.log(topicArn);
+
   const params = {
     TopicArn: topicArn,
     Message: body.message,
   };
 
+  console.log(JSON.stringify(params));
   const sns = new SNS();
   await sns.publish(params).promise();
 
